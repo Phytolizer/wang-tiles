299802c270270c81bcfefcc0a5b3a40cc55aeea6
Kyle Coffey <me@phytolizer.dev>
2022-04-26 18:57:01 -0500

    basic image

--- main.nim
+++ main.nim
@@ -1,3 +1,7 @@
+import std/[
+  strformat,
+]
+
 const
   WIDTH = 64
   HEIGHT = 64
@@ -5,6 +9,18 @@ const
 proc main =
   let f = open("output.ppm", fmWrite)
   defer: f.close()
+
+  f.writeLine("P6")
+  f.writeLine(fmt"{WIDTH} {HEIGHT}")
+  f.writeLine("255")
+  for y in 0..<HEIGHT:
+    for x in 0..<WIDTH:
+      let r = x / WIDTH
+      let g = y / HEIGHT
+      let b = 0.5
+      f.write(chr(int(r * 255)))
+      f.write(chr(int(g * 255)))
+      f.write(chr(int(b * 255)))
 
 when isMainModule:
   main()
