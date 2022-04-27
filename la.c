f3e7cf5fed59aa14c18c513541062e21e4711dd8
Kyle Coffey <me@phytolizer.dev>
2022-04-27 17:31:29 -0500

    finish la.c

--- la.c
+++ la.c
@@ -1,3 +1,6 @@
+#include <math.h>
+#include <stddef.h>
+
 #define C_X 0
 #define C_Y 1
 #define C_Z 2
@@ -11,6 +14,10 @@
 #define C_B 2
 #define C_A 3
 
+float lerpf(float a, float b, float t) {
+	return a + t * (b - a);
+}
+
 typedef struct {
 	float c[2];
 } Vec2f;
@@ -22,3 +29,89 @@ typedef struct {
 typedef struct {
 	float c[4];
 } Vec4f;
+
+typedef Vec3f RGB;
+
+Vec2f vec2f(float x, float y) {
+	Vec2f v = {{x, y}};
+	return v;
+}
+
+Vec2f vec2fs(float x) {
+	return vec2f(x, x);
+}
+
+Vec2f vec2f_sum(Vec2f a, Vec2f b) {
+	for (size_t i = 0; i < 2; i++) {
+		a.c[i] += b.c[i];
+	}
+	return a;
+}
+
+Vec2f vec2f_sub(Vec2f a, Vec2f b) {
+	for (size_t i = 0; i < 2; i++) {
+		a.c[i] -= b.c[i];
+	}
+	return a;
+}
+
+Vec2f vec2f_mul(Vec2f a, Vec2f b) {
+	for (size_t i = 0; i < 2; i++) {
+		a.c[i] *= b.c[i];
+	}
+	return a;
+}
+
+Vec2f vec2f_div(Vec2f a, Vec2f b) {
+	for (size_t i = 0; i < 2; i++) {
+		a.c[i] /= b.c[i];
+	}
+	return a;
+}
+
+Vec2f vec2f_max(Vec2f a, Vec2f b) {
+	for (size_t i = 0; i < 2; i++) {
+		if (a.c[i] < b.c[i]) {
+			a.c[i] = b.c[i];
+		}
+	}
+	return a;
+}
+
+Vec2f vec2f_min(Vec2f a, Vec2f b) {
+	for (size_t i = 0; i < 2; i++) {
+		if (a.c[i] > b.c[i]) {
+			a.c[i] = b.c[i];
+		}
+	}
+	return a;
+}
+
+float vec2f_sqrlen(Vec2f a) {
+	float sqr_len = 0.0F;
+	for (size_t i = 0; i < 2; i++) {
+		sqr_len += a.c[i] * a.c[i];
+	}
+	return sqr_len;
+}
+
+Vec2f vec2f_lerp(Vec2f a, Vec2f b, Vec2f t) {
+	for (size_t i = 0; i < 2; i++) {
+		a.c[i] = lerpf(a.c[i], b.c[i], t.c[i]);
+	}
+	return a;
+}
+
+Vec2f vec2f_sqrt(Vec2f a) {
+	for (size_t i = 0; i < 2; i++) {
+		a.c[i] = sqrtf(a.c[i]);
+	}
+	return a;
+}
+
+Vec2f vec2f_pow(Vec2f b, Vec2f e) {
+	for (size_t i = 0; i < 2; i++) {
+		b.c[i] = powf(b.c[i], e.c[i]);
+	}
+	return b;
+}
