87dbec455202a5f3b831043626fa09cbf875140b
Kyle Coffey <me@phytolizer.dev>
2022-04-26 19:23:29 -0500

    more work

--- main.nim
+++ main.nim
@@ -3,24 +3,22 @@ import std/[
 ]
 
 const
-  WIDTH = 64
-  HEIGHT = 64
+  WIDTH = 128
+  HEIGHT = 128
 
 proc main =
   let f = open("output.ppm", fmWrite)
   defer: f.close()
 
   f.writeLine("P6")
-  f.writeLine(fmt"{WIDTH} {HEIGHT}")
-  f.writeLine("255")
+  f.writeLine(fmt"{WIDTH} {HEIGHT} 255")
   for y in 0..<HEIGHT:
     for x in 0..<WIDTH:
-      let r = x / WIDTH
-      let g = y / HEIGHT
-      let b = 0.5
-      f.write(chr(int(r * 255)))
-      f.write(chr(int(g * 255)))
-      f.write(chr(int(b * 255)))
+      let u = x / WIDTH
+      let v = y / HEIGHT
+      f.write(chr(int(u * 255)))
+      f.write(chr(int(v * 255)))
+      f.write(chr(0))
 
 when isMainModule:
   main()
