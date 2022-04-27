465eb19d51f32b1effed83e6150904519d4873ea
Kyle Coffey <me@phytolizer.dev>
2022-04-26 19:26:56 -0500

    refactor: "fragment shader"

--- main.nim
+++ main.nim
@@ -6,6 +6,9 @@ const
   WIDTH = 128
   HEIGHT = 128
 
+proc frag(u, v: float): tuple[r: float, g: float, b: float] =
+  return (u, v, 0.0)
+
 proc main =
   let f = open("output.ppm", fmWrite)
   defer: f.close()
@@ -16,9 +19,10 @@ proc main =
     for x in 0..<WIDTH:
       let u = x / WIDTH
       let v = y / HEIGHT
-      f.write(chr(int(u * 255)))
-      f.write(chr(int(v * 255)))
-      f.write(chr(0))
+      let (r, g, b) = frag(u, v)
+      f.write(chr(int(r * 255)))
+      f.write(chr(int(g * 255)))
+      f.write(chr(int(b * 255)))
 
 when isMainModule:
   main()
